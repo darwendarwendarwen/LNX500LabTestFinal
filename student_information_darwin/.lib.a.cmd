bb458c422f779350af99117604db2941669e40b0
darwendarwendarwen <darwin.dwin.luk@gmail.com>
2024-12-08 00:33:03 -0500

    First configuration run and completed

---
+++ student_information_darwin/.lib.a.cmd
@@ -0,0 +1,1 @@
+cmd_student_information_darwin/lib.a := rm -f student_information_darwin/lib.a; /home/darwin/x-tools/aarch64-seneca-linux-gnu/bin/aarch64-seneca-linux-gnu-ar rcs student_information_darwin/lib.a student_information_darwin/LNX500_Test.o
